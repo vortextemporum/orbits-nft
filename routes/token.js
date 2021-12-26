9b690ba1f89bfd3a56f20d40b31ce1f7a9419fff
vortextemporum <berk.ozd@gmail.com>
2021-12-26 17:09:58 +0300

    fix

--- routes/token.js
+++ routes/token.js
@@ -18,13 +18,13 @@ router.get('/token/:id', async function (req, res, next) {
             res.status(202)
             return
         }
-        // const cached = await mc.get(`token_${parseInt(id).toString()}`)
-        // console.log(cached)
-        // if (cached.value) {
-        //     res.status(200).json(JSON.parse(cached.value?.toString()))
-        //     isBusy = false;
-        //     return
-        // }
+        const cached = await mc.get(`token_${parseInt(id).toString()}`)
+        console.log(cached)
+        if (cached.value) {
+            res.status(200).json(JSON.parse(cached.value?.toString()))
+            isBusy = false;
+            return
+        }
         isBusy = true
         const hash = await contract.tokenHash(id)
         if (!hash) {
